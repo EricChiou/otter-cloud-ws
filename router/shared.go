d05088d50549c894cb0a0e02438e4a6698f5e259
EricChiou <miatlab@gmail.com>
2021-02-20 15:59:43 +0800

    add get shared file list api

--- router/shared.go
+++ router/shared.go
@@ -8,6 +8,7 @@ func initSharedAPI() {
 
 	// Get
 	get(groupName+"/folder", true, nil, controller.GetSharedFolder)
+	get(groupName+"/file/list/:id/:prefix", true, nil, controller.GetObjectList)
 
 	// Post
 	post(groupName+"/add", true, nil, controller.Add)
